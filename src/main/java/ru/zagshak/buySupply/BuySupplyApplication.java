4a63372a00e77175cb337ae8677ccbc4e6672213
Vladislav Zagidullin <wladzag999@gmail.com>
2019-09-19 11:56:35 +0300

    Initial commit

---
+++ src/main/java/ru/zagshak/buySupply/BuySupplyApplication.java
@@ -0,0 +1,13 @@
+package ru.zagshak.buySupply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuySupplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuySupplyApplication.class, args);
+	}
+
+}
